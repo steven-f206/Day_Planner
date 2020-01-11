b489faf5698d3de362abe4fecac12acc200ef9a0
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-01-10 22:33:46 -0500

    Fixed date issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     let timeTrackObject = {};
 
+        // 1. Check if local storage exists, if it doesn't, load preset data to array
         if (localStorage.getItem("timeTrackObject")) {
             timeTrackObject = JSON.parse(localStorage.getItem("timeTrackObject"));
         }else {
@@ -18,37 +19,16 @@ $(document).ready(function() {
             };
         };
 
-        // console.log(timeTrackObject);
-
+        // 2. Load data loaded from code under comment 1 into page
         $(".time-block").each(function() {
             $(this).find(".description textarea").val(timeTrackObject[$(this).attr("data-time")].value);
         });
 
-    $("#currentDay").html(moment().format('dddd') + ", " +moment().format("MMM Do YY").substring(0,5) + "th");
-    
-    // When a user clicks the save button data is saved to the objects and to local storage.
-    $("body").on('click', ".saveBtn", function(e) {
+    // 3. Using moment.js to show what day it is.
+    let dateString = moment().format('dddd') + ", " +moment().format("MMM do YY");
+    $("#currentDay").html(dateString.substring(0, dateString.length - 5) + "th");
 
-        // Set variables for calling data
-        let hour = $(this).closest(".time-block").attr("data-time");
-        let textValue = $(this).closest(".time-block").find(".description textarea").val();
-        // let day moment().format("MMM Do YY").substring(0.5);
-
-        // console.log(timeTrackObject[hour].time);
-        // console.log(timeTrackObject[hour].value);
-
-        // value is overridden by for the object's value
-        timeTrackObject[hour].value = textValue;
-
-        // Send value to local storage for later use
-        localStorage.setItem('timeTrackObject', JSON.stringify(timeTrackObject));
-
-    });
-
-
-
-
-    // This checks the hour of the current day to the hour represented in the HTML data-element to decide its background color
+    // 4. This checks the hour of the current day to the hour represented in the HTML data-element to decide its background color
     const m = moment();
     $.each($(".time-block"), function(index, value) {
         let dateHour = $(value).attr("data-time");
@@ -61,6 +41,20 @@ $(document).ready(function() {
         }
     });
 
+    // 5. When a user clicks the save button data is saved to the objects and to local storage.
+    $("body").on('click', ".saveBtn", function(e) {
 
-    
+        // Set variables for calling data
+        let hour = $(this).closest(".time-block").attr("data-time");
+        let textValue = $(this).closest(".time-block").find(".description textarea").val();
+        // let day moment().format("MMM Do YY").substring(0.5);
+
+        // value is overridden by for the object's value
+        timeTrackObject[hour].value = textValue;
+
+        // Send value to local storage for later use
+        localStorage.setItem('timeTrackObject', JSON.stringify(timeTrackObject));
+
+    });
+
     });
