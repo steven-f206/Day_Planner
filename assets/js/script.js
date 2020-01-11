69052858d3d3337e1cfdb3c977cdc9f4d811565d
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-01-11 09:25:00 -0500

    Updated response below to be smaller sized

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 
     // 1. Create html of the timeblocks dynamically
 
-    for (var i = 9; 1 <= 17; i++){
-        $('.container').append('<div class="row time-block" data-time="${i}"> \
+    for (var i = 9; i <= 17; i++){
+        $('.container').append(`<div class="row time-block" data-time="${i}"> \
         <div class="col-sm col-md-2 hour"> \
             <p>${i}AM</p> \
         </div> \
         <div class="col-sm col-md-8 d-flex description"> \
             <textarea></textarea> \
-            </div> \
-            <div class="col-sm col-md-2 saveBtn"> \
-                <i class="far fa-save fa-2x"></i> \
-            </div> \
-            </div>');
-    };
+        </div> \
+        <div class="col-sm col-md-2 saveBtn"> \
+            <i class="far fa-save fa-2x"></i> \
+        </div> \
+        </div>`);
+        }
 
     let timeTrackObject = {};
 
